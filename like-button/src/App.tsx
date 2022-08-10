4355b08744f908df739671f4820cfcae66264e85
kondo takeshi <chaspy@users.noreply.github.com>
2022-08-11 02:34:15 +0900

    like

--- like-button/src/App.tsx
+++ like-button/src/App.tsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">TypeScript is cool</header>
+      <header className="App-header">
+        <LikeButton />
+      </header>
     </div>
+  );
+}
+
+function LikeButton() {
+  const [count, setCount] = useState(999);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  return (
+    <span className="likeButton" onClick={handleClick}>
+      ♥ {count}
+    </span>
   );
 }
 
